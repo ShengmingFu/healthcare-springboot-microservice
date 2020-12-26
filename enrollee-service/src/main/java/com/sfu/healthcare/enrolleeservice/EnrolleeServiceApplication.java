82ae88ee7324909068ca58ffbc2064d71d0fda67
shengmingfu <shengming.f@gmail.com>
2020-12-25 19:29:34 -0500

    commit for starting new adding

--- enrollee-service/src/main/java/com/sfu/healthcare/enrolleeservice/EnrolleeServiceApplication.java
+++ enrollee-service/src/main/java/com/sfu/healthcare/enrolleeservice/EnrolleeServiceApplication.java
@@ -3,12 +3,11 @@ package com.sfu.healthcare.enrolleeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
-@EnableDiscoveryClient
+
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EnrolleeServiceApplication {
 
     public static void main(String[] args) {
